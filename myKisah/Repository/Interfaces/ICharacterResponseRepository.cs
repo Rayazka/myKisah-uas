39a4fa5e2044d9dafe468111a10d7f4cf13b868e
raflyputra190 <raflyputra190@gmail.com>
2026-05-12 15:24:22 +0700

    test unit

--- myKisah/Repository/Interfaces/ICharacterResponseRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using myKisah.Models;
-
-namespace myKisah.Repository.Interfaces
-{
-    public interface ICharacterResponseRepository : IRepository<CharacterResponse>
-    {
-        IEnumerable<CharacterResponse> GetByMood(string characterId, MoodType mood);
-    }
-}
